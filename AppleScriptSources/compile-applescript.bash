e4e2f1665330d20ab0678990853170006fb0e9a7
Nathan Grigg <nathan@nathanamy.org>
2012-01-23 07:56:20 -0800

    Reorganized Script folder

--- AppleScriptSources/compile-applescript.bash
+++ AppleScriptSources/compile-applescript.bash
@@ -6,3 +6,10 @@ for f in *.applescript
 do
 	osacompile -o "../Contents/Scripts/${f%.applescript}.scpt" "$f"
 done
+
+cd "Latex Tools"
+
+for f in *.applescript
+do
+	osacompile -o "../../Contents/Scripts/Latex Tools/${f%.applescript}.scpt" "$f"
+done
