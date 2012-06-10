bd31a50052565b2bb6f4cc7624279d6aec82ac1b
Nathan Grigg <nathan@nathanamy.org>
2012-06-10 12:03:44 -0700

    Remove AppleScript binaries; add build script
    As discussed in Issue #2

--- AppleScriptSources/compile-applescript.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd $( dirname "${BASH_SOURCE[0]}" )
-
-find . -name "*.applescript" -print0 | while read -d $'\0' file
-do
-    osacompile -o "../Contents/${file%applescript}scpt" "$file"
-done
-
